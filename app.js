082f95811135b4c93f6f5ac9eb7e7e782712c7c6
Raymond <37949023+raytru@users.noreply.github.com>
2021-08-10 15:45:11 +1000

    burger menu fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-list');
-    // Toggle Nav
-    burger.addEventListener('click',()=> {
-        nav.classList.toggle('nav-active');
+const navMenu = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-list");
+  // Toggle Nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
-        // Burger animation
-        burger.classList.toggle('toggle')
-    });
-}
+    // Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
 
-navSlide();
+navMenu();
